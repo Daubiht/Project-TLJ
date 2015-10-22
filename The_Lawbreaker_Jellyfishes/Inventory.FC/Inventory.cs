57e8cad4a086618556f38446dddd840feed9ebc6
Duval <daubihtmania@gmail.com>
2015-10-22 21:50:14 +0200

    Functionality items, inventory

--- The_Lawbreaker_Jellyfishes/Inventory.FC/Inventory.cs
+++ The_Lawbreaker_Jellyfishes/Inventory.FC/Inventory.cs
@@ -5,7 +5,66 @@ using System.Text;
 
 namespace Inventory.FC
 {
+    /// <summary>
+    /// Class that manage an inventory
+    /// </summary>
     public class Inventory
     {
+        readonly Dictionary<Item, int> _inventory = new Dictionary<Item, int>();
+        int _weight = 100;
+        int _gold = 0;
+
+        public Item AddItem(Item item, int quantity)
+        {
+            int additional = 0;
+            foreach (Item items in _inventory.Keys)
+            {
+                additional += items.GetWeight;
+            }
+
+            if (additional < _weight)
+            {
+                if (_inventory.ContainsKey(item))
+                {
+                    _inventory[item] = quantity;
+                }
+                else
+                {
+                    _inventory.Add(item, quantity);
+                }
+
+                return item;
+            }
+            return null;
+        }
+
+        public bool RemoveItem(Item item)
+        {
+            if(_inventory[item] > 1)
+            {
+                _inventory[item]--;
+                return true;
+            }
+            else if(_inventory[item] < 1)
+            {
+                return _inventory.Remove(item);
+            }
+            return false;
+        }
+
+        public int RemoveGold(int RemoveGold)
+        {
+            return _gold -= RemoveGold;
+        }
+
+        public int AddGold(int AddGold)
+        {
+            return _gold += AddGold;
+        }
+
+        public int GetGold
+        {
+            get { return _gold; }
+        }
     }
 }
---
+++ The_Lawbreaker_Jellyfishes/Inventory.FC/item.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventory.FC
+{
+    /// <summary>
+    /// Class that creates objet item with a name, weight, value, description, bonus stat and required stat
+    /// </summary>
+    public class Item
+    {
+        readonly string _name;
+        readonly int _weight;
+        readonly int _value;
+        readonly string _description;
+        readonly Dictionary<string, int> _stats = new Dictionary<string, int>();
+        readonly Dictionary<string, int> _required = new Dictionary<string, int>();
+
+        public Item(string name, int weight, int value, string description, int statsNumbers, string statsNames, string requiredNames, int requiredNumbers)
+        {
+            _name = name;
+            _weight = weight;
+            _value = value;
+            _description = description;
+            _stats.Add(statsNames, statsNumbers);
+            _required.Add(requiredNames, requiredNumbers);
+        }
+
+        public string GetName
+        {
+            get { return _name; }
+        }
+
+        public int GetWeight
+        {
+            get { return _weight; }
+        }
+
+        public int GetValue
+        {
+            get { return _value; }
+        }
+
+        public string GetDescription
+        {
+            get { return _description; }
+        }
+
+        public Dictionary<string, int> GetStatsBonus
+        {
+            get { return _stats; }
+        }
+
+        public Dictionary<string, int> GetRequired
+        {
+            get { return _required; }
+        }
+    }
+}
