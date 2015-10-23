1b13633fea7404f6ac1b2f7ddd2c7a4688c1f136
Duval <daubihtmania@gmail.com>
2015-10-23 15:06:42 +0200

    Add and modification

--- The_Lawbreaker_Jellyfishes/Map/MapCity.cs
+++ The_Lawbreaker_Jellyfishes/Map/MapCity.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Map
 {
-    class MapCity
+    class MapIsland
     {
     }
 }
--- The_Lawbreaker_Jellyfishes/Map/MapWorld.cs
+++ The_Lawbreaker_Jellyfishes/Map/MapWorld.cs
@@ -6,16 +6,34 @@ using System.Text;
 namespace Map
 {
     /// <summary>
-    /// Repertory each islands and racheable islands
+    /// Repertory each islands and reachable islands
     /// Need to fill it with a file that contains every islands
     /// </summary>
     public class MapWorld
     {
 
-        readonly Dictionary<MapCity, MapCity[]> _islands = new Dictionary<MapCity, MapCity[]>();
+        readonly Dictionary<MapIsland, MapIsland[]> _islands = new Dictionary<MapIsland, MapIsland[]>();
         MapIsland _actualIsland;
 
-        
+        public MapWorld(MapIsland island, MapIsland[] listReachable)
+        {
+
+        }
+
+        public MapIsland ActualIsland
+        {
+            get { return _actualIsland; }
+            set
+            {
+                for(int i =0; i < _islands[_actualIsland].Length; i++)
+                {
+                    if (_islands[_actualIsland][i] == value)
+                    {
+                        _actualIsland = value;
+                    }
+                }
+            }
+        }
 
     }
 }
