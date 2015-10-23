9d18cf2b154bd18c697424bd2d44f55d8761cdb4
Duval <daubihtmania@gmail.com>
2015-10-23 14:49:16 +0200

    add and modification

--- The_Lawbreaker_Jellyfishes/Map/MapWorld.cs
+++ The_Lawbreaker_Jellyfishes/Map/MapWorld.cs
@@ -5,7 +5,17 @@ using System.Text;
 
 namespace Map
 {
+    /// <summary>
+    /// Repertory each islands and racheable islands
+    /// Need to fill it with a file that contains every islands
+    /// </summary>
     public class MapWorld
     {
+
+        readonly Dictionary<MapCity, MapCity[]> _islands = new Dictionary<MapCity, MapCity[]>();
+        MapIsland _actualIsland;
+
+        
+
     }
 }
