de454b78076d9f7aac3d11fa84070401d0d9e92e
Duval <daubihtmania@gmail.com>
2015-10-25 20:00:08 +0100

    Create notification system and unit test

---
+++ The_Lawbreaker_Jellyfishes/Notification/ListNotifications.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notification
+{
+    public class ListNotifications
+    {
+        readonly List<Notification> _listNotif = new List<Notification>();
+
+        public Notification AddNotif(string name, string description)
+        {
+            Notification newNotif = new Notification(name, description);
+            _listNotif.Add(newNotif);
+
+            if (_listNotif.Count == 11) this.RemoveNotif();
+
+            return newNotif;
+        }
+
+        private bool RemoveNotif()
+        {
+            _listNotif.RemoveAt(0);
+            return true;
+        }
+
+        public List<Notification> ListNotif
+        {
+            get { return _listNotif; }
+        }
+    }
+}
---
+++ The_Lawbreaker_Jellyfishes/Notification/notification.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notification
+{
+    public class Notification
+    {
+        readonly string _name;
+        readonly string _description;
+        readonly DateTime _date;
+
+        internal Notification(string name, string description)
+        {
+            _name = name;
+            _description = description;
+            _date = DateTime.Now;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+        }
+    }
+}
