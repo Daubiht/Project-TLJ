f76235fff070d6bd09b4198af05b63917be040b2
jerome tea <jerometea@gmail.com>
2015-10-27 14:43:26 +0100

    Creation of the class "Merchant"

---
+++ The_Lawbreaker_Jellyfishes/LogicalGame/Merchant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Inventory.FC;
+
+namespace LogicalGame
+{
+    class Merchant
+    {
+        ListItems _listOfAllItems;
+        Team _currentTeam;
+        City _contextCity;
+
+
+        // Constructor
+        public Merchant(ListItems listOfAllItems, Team currentTeam, City contextCity)
+        {
+            _listOfAllItems = listOfAllItems;
+            _currentTeam = currentTeam;
+            _contextCity = contextCity;
+        }
+
+        // Methodes
+
+        // Buy an item
+        public void BuyItems(Item ItemBought, int quantity)
+        {
+            _currentTeam.InventTeam.AddItem(ItemBought, quantity);
+        }
+
+        // Sell an item
+        public void SellItems(Item ItemSold)
+        {
+            _currentTeam.InventTeam.AddGold(ItemSold.GetValue);
+            _currentTeam.InventTeam.RemoveItem(ItemSold);
+        }
+
+    }
+}
