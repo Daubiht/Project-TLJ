81ba3629ba18c864316a03a5af9365a974e2213f
Duval <daubihtmania@gmail.com>
2015-11-03 21:47:50 +0100

    done

--- The_Lawbreaker_Jellyfishes/LogicalGame/Maps/MapCity.cs
+++ The_Lawbreaker_Jellyfishes/LogicalGame/Maps/MapCity.cs
@@ -10,36 +10,16 @@ namespace LogicalGame
     {
         readonly MapIsland _context;
         readonly string _name;
-        readonly List<Services> _listServices;
-        Services _actualService;
 
         public MapCity(MapIsland context, string name)
         {
             _name = name;
             _context = context;
-            _listServices = listServices;
         }
 
         public string CityName
         {
             get { return _name; }
-        }
-
-        public List<Services> listServices
-        {
-            get { return _listServices; }
-        }
-
-        public Services ActualService
-        {
-            get { return _actualService; }
-            set
-            {
-                if (_context.ActualPlace == this)
-                {
-                    _actualService = value;
-                }
-            }
         }
 
         public MapIsland ActualIsland
