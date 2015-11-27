d51d5b6e9486adef3420f340cdc1fbb0bf2f7af9
Duval <daubihtmania@gmail.com>
2015-11-27 12:59:04 +0100

    save

--- The_Lawbreaker_Jellyfishes/LogicalGame/Maps/MapZone.cs
+++ The_Lawbreaker_Jellyfishes/LogicalGame/Maps/MapZone.cs
@@ -8,6 +8,7 @@ namespace LogicalGame
     {
         List<MapZone> _listLink = new List<MapZone>();
         MapInstance _context;
+        int _zoneLevel;
 
         public MapZone(MapInstance context, bool WithInstance)
         {
@@ -29,5 +30,37 @@ namespace LogicalGame
             set { _listLink = value; }
         }
 
+        public object Event()
+        {
+            Random rand = new Random();
+            int result = rand.Next(0, 2);
+
+            if (result == 0)
+            {
+                ListMonsters EntirelistM = new ListMonsters();
+                List<Monster> listMByLevel = EntirelistM.GetListMonsters.FindAll(
+                    delegate (Monster m)
+                    {
+                        return m.Level > (_zoneLevel - 3) && m.Level < (_zoneLevel + 3);
+                    }
+                );
+
+                int nbrM = rand.Next(1, 5);
+                List<Monster> listMForFight = new List<Monster>();
+                for(int i = 0; i < nbrM; i ++)
+                {
+                    int wanted = rand.Next(0, listMByLevel.Count);
+                    listMForFight.Add(listMByLevel[wanted]);
+                    listMByLevel.RemoveAt(wanted);
+                }
+                return listMForFight;
+
+            }
+            else if(result == 1)
+            {
+
+            }
+
+        }
     }
 }
