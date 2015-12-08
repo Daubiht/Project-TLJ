11f9dbd972afaff601bf3a943e1d510804e2e3ab
Scarsnik <leo.stefani@wanadoo.fr>
2015-12-08 22:32:48 +0100

    Correction of bugs with the SkillsTree

--- The_Lawbreaker_Jellyfishes/GraphicalInterface/Menu/Team/SkillsTree.cs
+++ The_Lawbreaker_Jellyfishes/GraphicalInterface/Menu/Team/SkillsTree.cs
@@ -30,21 +30,50 @@ namespace GraphicalInterface
             Button button = (Button)sender;
             Skill skill = (Skill)button.Tag;
 
-            if (_chara.AddSkill(skill.Name, skill) == null)
+            if (_chara.SkillPoint <= 0)
+            {
+                LError.Text = "Vous n'avez pas assez de point de compétence pour apprendre ceci";
+                LError.Visible = true;
+            }
+            else if (_chara.AddSkill(skill.Name, skill) != null)
+            {
+                ((SkillsDisplay)(Parent.Parent.Parent)).SkillPoint.Text = _chara.SkillPoint + " restant";
+                Reload();
+            }
+            else
             {
                 LError.Text = "Impossible d'apprendre cette compétence";
                 LError.Visible = true;
             }
 
-            ((SkillsDisplay)(Parent.Parent.Parent)).SkillPoint.Text = _chara.SkillPoint + " restant";
+            
 
-            Reload();
+        }
 
+        private string Infos (Skill skill)
+        {
+            string info;
+
+            info = skill.Description + Environment.NewLine;
+            if (skill.Cost[1] > 0)
+            {
+                info += "Fatigue : " + skill.Cost[1] + Environment.NewLine;
+            }
+            if (skill.Cost[0] > 0)
+            {
+                info += "Vie : " + skill.Cost[0] + Environment.NewLine;
+            }
+
+            return info;
         }
 
         private void Reload ()
         {
             SkillList sl = new SkillList();
+            ToolTip toolTip = new ToolTip();
+            string info = "";
+
+            ((SkillsDisplay)(Parent.Parent.Parent)).SkillPoint.Text = _chara.SkillPoint + " restant";
 
             PanelToCenter.Controls.Clear();
             usedSkills.Clear();
@@ -107,6 +136,7 @@ namespace GraphicalInterface
                             BSkill.Left = (k - 1) * (BSkill.Width + 5);
                             BSkill.Text = usedSkills[i][j][k].Name;
                             BSkill.Tag = usedSkills[i][j][k];
+                            toolTip.SetToolTip(BSkill, Infos(usedSkills[i][j][k]));
                             if ((usedSkills[i][j][k].PreviousSkill != null && !_chara.Skills.ContainsKey(usedSkills[i][j][k].PreviousSkill.Name)) || _chara.Stats[stat] < usedSkills[i][j][k].statRequired[stat])
                             {
                                 BSkill.Enabled = false;
@@ -160,6 +190,7 @@ namespace GraphicalInterface
                                 newButton.Left = (maxX + minX) / 2 - newButton.Width / 2;
                                 newButton.Text = usedSkills[i][j][k].Name;
                                 newButton.Tag = usedSkills[i][j][k];
+                                toolTip.SetToolTip(newButton, Infos(usedSkills[i][j][k]));
                                 if ((usedSkills[i][j][k].PreviousSkill != null && !_chara.Skills.ContainsKey(usedSkills[i][j][k].PreviousSkill.Name)) || _chara.Stats[stat] < usedSkills[i][j][k].statRequired[stat])
                                 {
                                     newButton.Enabled = false;
@@ -204,6 +235,7 @@ namespace GraphicalInterface
                                 newButton.Left = maxX + 40;
                                 newButton.Text = usedSkills[i][j][k].Name;
                                 newButton.Tag = usedSkills[i][j][k];
+                                toolTip.SetToolTip(newButton, Infos(usedSkills[i][j][k]));
                                 if ((usedSkills[i][j][k].PreviousSkill != null && !_chara.Skills.ContainsKey(usedSkills[i][j][k].PreviousSkill.Name)) || _chara.Stats[stat] < usedSkills[i][j][k].statRequired[stat])
                                 {
                                     newButton.Enabled = false;
