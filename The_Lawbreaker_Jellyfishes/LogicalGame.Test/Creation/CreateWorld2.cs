c81046f5a504d57cc6f673fb3509e38f1a95b228
Duval <daubihtmania@gmail.com>
2016-01-14 21:34:05 +0100

    modified world

--- The_Lawbreaker_Jellyfishes/LogicalGame.Test/Creation/CreateWorld2.cs
+++ The_Lawbreaker_Jellyfishes/LogicalGame.Test/Creation/CreateWorld2.cs
@@ -20,28 +20,32 @@ namespace LogicalGame.Test
             Dictionary<string, MapIsland> Islands = new Dictionary<string, MapIsland>();
 
             //island 1 ------------------------------------------------------------
-            MapIsland island = new MapIsland(world, "island1");
-            island.PointX = 118;
-            island.PointY = 31;
+            MapIsland island = new MapIsland(world, "Poniénne");
+            island.PointX = 0;
+            island.PointY = 0;
 
             //Instance for the Island
-            MapInstance instance1 = new MapInstance(island, "island1_instance_1", false);
-            instance1.PointX = 130;
-            instance1.PointY = 50;
+            MapInstance instance1 = new MapInstance(island, "Bois polaire", false);
+            instance1.PointX = 0;
+            instance1.PointY = 0;
 
             //list zone for instance on the island
             List<MapZone> listZone_instance1 = new List<MapZone>();
             MapZone zone1 = new MapZone(instance1, true, 1);
-            zone1.PointX = 50;
-            zone1.PointY = 50;
+            zone1.PointX = 0;
+            zone1.PointY = 0;
 
             MapZone zone2 = new MapZone(instance1, true, 2);
-            zone2.PointX = 150;
-            zone2.PointY = 150;
+            zone2.PointX = 50;
+            zone2.PointY = 0;
 
             MapZone zone3 = new MapZone(instance1, true, 3);
-            zone3.PointX = 250;
-            zone3.PointY = 250;
+            zone3.PointX = 100;
+            zone3.PointY = 0;
+
+            MapZone zone4 = new MapZone(instance1, true, 3);
+            zone3.PointX = 150;
+            zone3.PointY = 0;
 
             List<MapZone> listlink = new List<MapZone>();
             listlink.Add(zone2);
@@ -54,52 +58,188 @@ namespace LogicalGame.Test
 
             listlink = new List<MapZone>();
             listlink.Add(zone2);
+            listlink.Add(zone4);
             zone3.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone3);
+            zone4.ListLink = listlink;
 
             //Zone must be add like : first is the entry and last the exit
             listZone_instance1.Add(zone1);
             listZone_instance1.Add(zone2);
             listZone_instance1.Add(zone3);
+            listZone_instance1.Add(zone4);
 
             instance1.listZones = listZone_instance1;
 
+            MapInstance instance2 = new MapInstance(island, "Carverne écailleuse", false);
+            instance1.PointX = 50;
+            instance1.PointY = 0;
+
+            //list zone for instance on the island
+            List<MapZone> listZone_instance2 = new List<MapZone>();
+            zone1 = new MapZone(instance2, true, 1);
+            zone1.PointX = 0;
+            zone1.PointY = 0;
+
+            zone2 = new MapZone(instance2, true, 2);
+            zone2.PointX = 50;
+            zone2.PointY = 0;
+
+            zone3 = new MapZone(instance2, true, 3);
+            zone3.PointX = 100;
+            zone3.PointY = 0;
+
+            MapZone zone3_1 = new MapZone(instance2, true, 3);
+            zone3.PointX = 100;
+            zone3.PointY = 50;
+
+            zone4 = new MapZone(instance2, true, 3);
+            zone3.PointX = 150;
+            zone3.PointY = 0;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone2);
+            zone1.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone1);
+            listlink.Add(zone3);
+            zone2.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone2);
+            listlink.Add(zone3_1);
+            listlink.Add(zone4);
+            zone3.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone3);
+            zone3_1.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone3);
+            zone4.ListLink = listlink;
+
+            //Zone must be add like : first is the entry and last the exit
+            listZone_instance2.Add(zone1);
+            listZone_instance2.Add(zone2);
+            listZone_instance2.Add(zone3);
+            listZone_instance2.Add(zone3_1);
+            listZone_instance2.Add(zone4);
+
+            instance2.listZones = listZone_instance2;
+
+            //Instance for the Island
+            MapInstance instance3 = new MapInstance(island, "Ancien mégalithe", false);
+            instance3.PointX = 150;
+            instance3.PointY = 0;
+
+            //list zone for instance on the island
+            List<MapZone> listZone_instance3 = new List<MapZone>();
+            zone1 = new MapZone(instance3, true, 1);
+            zone1.PointX = 0;
+            zone1.PointY = 0;
+
+            zone2 = new MapZone(instance3, true, 2);
+            zone2.PointX = 50;
+            zone2.PointY = 0;
+
+            MapZone zone2_1 = new MapZone(instance3, true, 2);
+            zone2.PointX = 50;
+            zone2.PointY = 50;
+
+            zone3 = new MapZone(instance3, true, 3);
+            zone3.PointX = 100;
+            zone3.PointY = 0;
+
+            zone3_1 = new MapZone(instance3, true, 3);
+            zone3_1.PointX = 100;
+            zone3_1.PointY = 50;
+
+            zone4 = new MapZone(instance3, true, 3);
+            zone3.PointX = 150;
+            zone3.PointY = 0;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone2);
+            zone1.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone1);
+            listlink.Add(zone2_1);
+            listlink.Add(zone3);
+            zone2.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone2);
+            listlink.Add(zone3_1);
+            listlink.Add(zone4);
+            zone3.ListLink = listlink;
+
+            listlink = new List<MapZone>();
+            listlink.Add(zone3);
+            zone4.ListLink = listlink;
+
+            //Zone must be add like : first is the entry and last the exit
+            listZone_instance3.Add(zone1);
+            listZone_instance3.Add(zone2);
+            listZone_instance3.Add(zone2_1);
+            listZone_instance3.Add(zone3);
+            listZone_instance3.Add(zone3_1);
+            listZone_instance3.Add(zone4);
+
+            instance3.listZones = listZone_instance3;
+
             List<MapInstance> listinstances = new List<MapInstance>();
             listinstances.Add(instance1);
+            listinstances.Add(instance2);
+            listinstances.Add(instance3);
             island.IslandInstances = listinstances;
 
             //city for the island
-            MapCity city = new MapCity(island, "City1");
-            city.PointX = 100;
-            city.PointY = 200;
+            MapCity city = new MapCity(island, "Ponyo");
+            city.PointX = 200;
+            city.PointY = 0;
             island.AddCity(city);
 
             //list sellable items for merchant and merch
             ListItems listI = new ListItems();
-            List<Item> itemsSell = new List<Item>();
-            itemsSell.Add(listI.Items[0]);
+
+            List<Item> itemsSell = listI.Items.FindAll
+                (delegate (Item I)
+                {
+                    return I.GetName == "Dague en bois" || I.GetName == "Bandage";
+                });
 
             Merchant merch1 = new Merchant("Vendeur général", itemsSell);
             merch1.PointX = 50;
-            merch1.PointY = 325;
+            merch1.PointY = 50;
             merch1.Invent = world.Team.Invent;
 
-            itemsSell.Add(listI.Items[1]);
+            itemsSell = listI.Items.FindAll
+                (delegate (Item I)
+                {
+                    return I.GetName == "Bandage" || I.GetName == "Poils animals";
+                });
+
             Merchant merch2 = new Merchant("herboriste", itemsSell);
-            merch2.PointX = 250;
-            merch2.PointY = 325;
+            merch2.PointX = 100;
+            merch2.PointY = 50;
             merch2.Invent = world.Team.Invent;
 
             //Add services
             Mortuary s1 = new Mortuary(city);
-            s1.PointX = 300;
-            s1.PointY = 110;
+            s1.PointX = 50;
+            s1.PointY = 100;
 
             Taverne s2 = new Taverne();
-            s2.PointX = 190;
+            s2.PointX = 100;
             s2.PointY = 100;
 
             Militia s3 = new Militia(city);
-            s3.PointX = 100;
+            s3.PointX = 150;
             s3.PointY = 100;
 
             //list merchant for city
